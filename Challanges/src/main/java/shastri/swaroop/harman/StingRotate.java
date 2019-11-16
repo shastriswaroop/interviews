b01102735de08b90e346f927aff1b02bb6c16d78
shastriswaroop <shastriswaroop@gmail.com>
2019-11-16 19:59:40 +0530

    Update StingRotate.java

--- Challanges/src/main/java/shastri/swaroop/harman/StingRotate.java
+++ Challanges/src/main/java/shastri/swaroop/harman/StingRotate.java
@@ -5,5 +5,36 @@ package shastri.swaroop.harman;
 // string "dabc" is rotation
 // string "adbc" is not
 public class StingRotate {
-
+	public static void main(String args[]) {
+		String str1 ="abcd";
+//		String str2 = "cbda";
+		String str2 = "dabc";
+		
+		if(str1==null ||  str2==null) {
+			System.out.println("False");
+			
+		} else {
+			
+			if(str1.length()!=str2.length()) {
+				System.out.println("False");
+			} else {
+				String lStr, rStr, fStr;
+				int count = 1;
+				while(count<str2.length()) {
+					lStr	= str2.substring(count);
+					rStr = str2.substring(0,count);
+					count++;
+//					System.out.println(lStr + " , "+ rStr);
+					
+					if(str1.equalsIgnoreCase(lStr+rStr)) {
+						System.out.println("strings can be converted to each other");
+						break;
+					}
+				}
+				
+			}
+			
+		}
+		
+	}
 }
